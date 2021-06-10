3cf0e45470ef0306acaf79d0184245c8a6ad40b1
kofoednielsen <asbjorn.ko.ni@gmail.com>
2021-06-10 23:09:03 +0200

    New pipeline changes! |

--- droneops.sh
+++ droneops.sh
@@ -1,8 +1,12 @@
 while true then 
   do
-  echo "HTTP/1.1 200 OK" | nc -lN 1234
+  echo "HTTP/1.1 200 OK" | nc -lN 1234 | /dev/null
   echo "Running pipeline"
+  echo "Pulling newest version.."
   git pull origin main
+  echo "Compiling.."
   arduino-cli compile --fqbn arduino:avr:uno blink
+  echo "Uploading.."
   arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno blink
+  echo "Done."
 done

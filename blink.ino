d8332a65cb44b2f1859fa0e514b928d9d9462222
kofoednielsen <asbjorn.ko.ni@gmail.com>
2021-07-19 22:21:22 +0200

    random change

--- blink.ino
+++ blink.ino
@@ -1,12 +1,6 @@
-int MY_PIN = 8;
-
 void setup() {
    pinMode(LED_BUILTIN, OUTPUT);
-   pinMode(MY_PIN, INPUT);
-   Serial.begin(9600);
-   Serial.println("Running!");  
  }
- void loop() {
-   boolean my_pin_state = digitalRead(MY_PIN);
-   digitalWrite(LED_BUILTIN, my_pin_state);
+void loop() {
+   digitalWrite(LED_BUILTIN, HIGH);
 }

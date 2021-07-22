fe780073fa9902721f649dda172e05bd3f1bd7e8
kofoednielsen <asbjorn.ko.ni@gmail.com>
2021-07-22 23:56:14 +0200

    Sleep is for the weak

--- blink.ino
+++ blink.ino
@@ -109,14 +109,12 @@ void loop() {
 
     #ifdef OUTPUT_READABLE_ACCELGYRO
         // display tab-separated accel/gyro x/y/z values
-        Serial.print("a/g:\t");
         // Serial.print(ax); Serial.print("\t");
         // Serial.print(ay); Serial.print("\t");
         // Serial.print(az); Serial.print("\t");
         Serial.print(gx); Serial.print("\t");
         Serial.print(gy); Serial.print("\t");
         Serial.println(gz);
-        delay(50);
     #endif
 
     #ifdef OUTPUT_BINARY_ACCELGYRO

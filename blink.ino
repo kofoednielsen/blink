3763c864f69ce3df52633ab1ca0a554e5e2d21b5
kofoednielsen <asbjorn.ko.ni@gmail.com>
2021-07-25 13:16:08 +0200

    probably broke everything

--- blink.ino
+++ blink.ino
@@ -1,78 +1,27 @@
-// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
-// for both classes must be in the include path of your project
 #include "I2Cdev.h"
 #include "MPU6050.h"
+#include "Wire.h"
 
-// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
-// is used in I2Cdev.h
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-    #include "Wire.h"
-#endif
-
-// class default I2C address is 0x68
-// specific I2C addresses may be passed as a parameter here
-// AD0 low = 0x68 (default for InvenSense evaluation board)
-// AD0 high = 0x69
 MPU6050 accelgyro;
-//MPU6050 accelgyro(0x69); // <-- use for AD0 high
 
-int16_t ax, ay, az;
 int16_t gx, gy, gz;
 
-
-
-// uncomment "OUTPUT_READABLE_ACCELGYRO" if you want to see a tab-separated
-// list of the accel X/Y/Z and then gyro X/Y/Z values in decimal. Easy to read,
-// not so easy to parse, and slow(er) over UART.
-#define OUTPUT_READABLE_ACCELGYRO
-
-// uncomment "OUTPUT_BINARY_ACCELGYRO" to send all 6 axes of data as 16-bit
-// binary, one right after the other. This is very fast (as fast as possible
-// without compression or data loss), and easy to parse, but impossible to read
-// for a human.
-//#define OUTPUT_BINARY_ACCELGYRO
-
-
-#define LED_PIN 13
-bool blinkState = false;
-
 void setup() {
-    // join I2C bus (I2Cdev library doesn't do this automatically)
-    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-        Wire.begin();
-    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-        Fastwire::setup(400, true);
-    #endif
+  Wire.begin();
 
-    accelgyro.initialize();
+  accelgyro.initialize();
 
-    // use the code below to change accel/gyro offset values
-    accelgyro.setXGyroOffset(-43);
-    accelgyro.setYGyroOffset(33);
-    accelgyro.setZGyroOffset(-28);
-
-    // configure Arduino LED pin for output
-    pinMode(LED_PIN, OUTPUT);
+  // use the code below to change accel/gyro offset values
+  accelgyro.setXGyroOffset(-43);
+  accelgyro.setYGyroOffset(33);
+  accelgyro.setZGyroOffset(-28);
 }
 
 unsigned long last_time = 0;
 int16_t delta_t = 0;
 
 void loop() {
-    // read raw accel/gyro measurements from device
     accelgyro.getRotation(&gx, &gy, &gz);
-
-    // these methods (and a few others) are also available
-    //accelgyro.getAcceleration(&ax, &ay, &az);
-    //accelgyro.getRotation(&gx, &gy, &gz);
-
-    // display tab-separated accel/gyro x/y/z values
-    // Serial.print(ax); Serial.print("\t");
-    // Serial.print(ay); Serial.print("\t");
-    // Serial.print(az); Serial.print("\t");
-    // Serial.print(gx); Serial.print("\t");
-    // Serial.print(gy); Serial.print("\t");
-    // Serial.println(gz);
 
     delta_t = micros()-last_time;
     Serial.write((uint8_t)(gx & 0xFF)); Serial.write((uint8_t)(gx >> 8));
@@ -80,9 +29,7 @@ void loop() {
     Serial.write((uint8_t)(gz & 0xFF)); Serial.write((uint8_t)(gz >> 8));
     Serial.write((uint8_t)(delta_t & 0xFF)); Serial.write((uint8_t)(delta_t >> 8));
 
-    // blink LED to indicate activity
-    digitalWrite(LED_PIN, blinkState);
-    blinkState = !blinkState;
+    // fly
 
     last_time = micros();
 }

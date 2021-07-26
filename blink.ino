e5c27dce00dd4f226ad4a649281de00b843c326d
Mads D. Pedersen <mdamgaard@biolib.com>
2021-07-26 23:36:54 +0200

    Read all the things (Remember to install the rc lib Asbjørn)

--- blink.ino
+++ blink.ino
@@ -1,10 +1,17 @@
 #include "I2Cdev.h"
 #include "MPU6050.h"
 #include "Wire.h"
+#include <RcChannel.h>
+// #include <Servo.h>
 
 MPU6050 accelgyro;
 
 int16_t gx, gy, gz;
+
+// servo motor_1;
+// servo motor_2;
+// servo motor_3;
+// servo rudder; 
 
 void setup() {
   Wire.begin();
@@ -15,6 +22,19 @@ void setup() {
   accelgyro.setXGyroOffset(-43);
   accelgyro.setYGyroOffset(33);
   accelgyro.setZGyroOffset(-28);
+
+  // Setup receiver channels with default min, mid & max (1000, 1500, 1900)
+  RcChannel rc[] = {1, 2, 3, 4};
+
+  // aileron.attach(5);
+  // aileron.writeMicroseconds(1500);
+  // elevator.attach(6);
+  // elevator.writeMicroseconds(1500);
+  // throttle.attach(7);
+  // throttle.writeMicroseconds(1500);
+  // rudder.attach(8);
+  // rudder.writeMicroseconds(1500);
+
 }
 
 unsigned long last_time = 0;
@@ -25,11 +45,22 @@ void loop() {
 
     delta_t = micros()-last_time;
     Serial.write((uint8_t)(gx & 0xFF)); Serial.write((uint8_t)(gx >> 8));
-    Serial.write((uint8_t)(gy & 0xFF)); Serial.write((uint8_t)(gy >> 8)); 
+    Serial.write((uint8_t)(gy & 0xFF)); Serial.write((uint8_t)(gy >> 8));
     Serial.write((uint8_t)(gz & 0xFF)); Serial.write((uint8_t)(gz >> 8));
     Serial.write((uint8_t)(delta_t & 0xFF)); Serial.write((uint8_t)(delta_t >> 8));
 
-    // fly
+    aileron_percentage = rc[1].readPercentage();
+    elevator_percentage = rc[2].readPercentage();
+    throttle_percentage = rc[3].readPercentage();
+    rudder_percentage = rc[4].readPercentage();
+
+    Serial.print(aileron_percentage);
+    Serial.print("\t");
+    Serial.print(elevator_percentage);
+    Serial.print("\t");
+    Serial.print(throttle_percentage);
+    Serial.print("\t");
+    Serial.print(rudder_percentage);
 
     last_time = micros();
 }

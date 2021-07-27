98b22aa51271228c7091e233e3e2fa2a7a83cd85
Mads D. Pedersen <mdamgaard@biolib.com>
2021-07-27 22:53:22 +0200

    With the right library this time

--- blink.ino
+++ blink.ino
@@ -1,7 +1,7 @@
 #include "I2Cdev.h"
 #include "MPU6050.h"
 #include "Wire.h"
-#include <RcChannel.h>
+#include "RCReceive.h"
 #include <Servo.h>
 
 MPU6050 accelgyro;
@@ -11,7 +11,12 @@ int16_t gx, gy, gz;
 servo motor_1;
 servo motor_2;
 // servo motor_3;
-// servo motor_4; 
+// servo motor_4;
+
+RCReceive aileron;
+RCReceive elevator;
+RCReceive throttle;
+RCReceive rudder;
 
 void setup() {
   Wire.begin();
@@ -24,7 +29,10 @@ void setup() {
   accelgyro.setZGyroOffset(-28);
 
   // Setup receiver channels with default min, mid & max (1000, 1500, 1900)
-  RcChannel rc[] = {1, 2, 3, 4};
+  aileron.attachInt(1);
+  elevator.attachInt(2);
+  throttle.attachInt(3);
+  rudder.attachInt(4);
 
   motor_1.attach(5);
   motor_1.writeMicroseconds(1500);
@@ -49,18 +57,18 @@ void loop() {
     Serial.write((uint8_t)(gz & 0xFF)); Serial.write((uint8_t)(gz >> 8));
     Serial.write((uint8_t)(delta_t & 0xFF)); Serial.write((uint8_t)(delta_t >> 8));
 
-    aileron_pwm = rc[1].readRaw();
-    elevator_pwm = rc[2].readRaw();
-    throttle_pwm = rc[3].readRaw();
-    rudder_pwm = rc[4].readRaw();
+    aileron_pwm = aileron.getValue();
+    elevator_pwm = elevator.getValue();
+    throttle_pwm = throttle.getValue();
+    rudder_pwm = rudder.getValue();
 
-    Serial.print(aileron_percentage);
+    Serial.print(aileron_pwm);
     Serial.print("\t");
-    Serial.print(elevator_percentage);
+    Serial.print(elevator_pwm);
     Serial.print("\t");
-    Serial.print(throttle_percentage);
+    Serial.print(throttle_pwm);
     Serial.print("\t");
-    Serial.print(rudder_percentage);
+    Serial.print(rudder_pwm);
 
     motor_1.writeMicroseconds(throttle_pwm);
     motor_2.writeMicroseconds(throttle_pwm);

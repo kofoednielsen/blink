c203bdc76877290030feb9e54d703792c4fc4c8a
kofoednielsen <asbjorn.ko.ni@gmail.com>
2021-08-03 20:14:34 +0200

    Nico thinks i'm un-optimized

--- blink.ino
+++ blink.ino
@@ -67,7 +67,7 @@ int loop_counter = 0; void loop() {
     motor_2.writeMicroseconds(1000);
    }
   if (loop_counter++ % 200 == 0) {
-    float tmp = ((float)throttle.getValue()-1000)/10000;
+    float tmp = abs(((float)throttle.getValue()-1000)/10000);
     myPID.configure(pk, 0, tmp, 1000, 16, true);
     Serial.print(tmp, 4);
     Serial.print("\t");

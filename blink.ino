d2d87a0d35eaab68fbb9ba200233d72a9c5d794a
kofoednielsen <asbjorn.ko.ni@gmail.com>
2021-08-04 19:57:56 +0200

    🤞

--- blink.ino
+++ blink.ino
@@ -10,18 +10,19 @@ int16_t gx, gy, gz;
 
 Servo motor_1;
 Servo motor_2;
-// servo motor_3;
-// servo motor_4;
+servo motor_3;
+servo motor_4;
 
 PWM aileron(1);
 PWM elevator(2);
 PWM throttle(3);
 PWM rudder(4);
 
-float pk = 0.0067;
+float pk = 0.0050;
 float dk = 0.0000057;
 
-FastPID myPID(pk, 0, dk, 1000, 16, true);
+FastPID xPID(pk, 0, dk, 1000, 16, true);
+FastPID yPID(pk, 0, dk, 1000, 16, true);
 
 void setup() {
   Wire.begin();
@@ -41,14 +42,10 @@ void setup() {
   throttle.begin(true);
   rudder.begin(true);
 
-  motor_1.attach(5);
-  motor_1.writeMicroseconds(1000);
-  motor_2.attach(6);
-  motor_2.writeMicroseconds(1000);
-  // motor_3.attach(7);
-  // motor_3.writeMicroseconds(1500);
-  // motor_4.attach(8);
-  // motor_4.writeMicroseconds(1500);
+  motor_1.attach(8);
+  motor_2.attach(5);
+  motor_3.attach(6);
+  motor_4.attach(7);
 }
 
 long last = 0;
@@ -57,21 +54,24 @@ int loop_counter = 0; void loop() {
   last = micros();
   accelgyro.getRotation(&gx, &gy, &gz);
 
-  int16_t calc_throttle = (elevator.getValue() - 500);
   //Serial.println(output);
-  if (calc_throttle > 1000) {
-    int16_t output = myPID.step(aileron.getValue() - 1500, gy);
-    motor_1.writeMicroseconds(calc_throttle - output);
-    motor_2.writeMicroseconds(calc_throttle + output);
+  if (throttle.getValue() > 1000) {
+    int16_t x_output = myPID.step(aileron.getValue() - 1500, gx);
+    int16_t y_output = myPID.step(elevator.getValue() - 1500, gy);
+    motor_1.writeMicroseconds(throttle.getValue() + y_output + x_output);
+    motor_2.writeMicroseconds(throttle.getValue() - y_output + x_output);
+    motor_3.writeMicroseconds(throttle.getValue() + y_output - x_output);
+    motor_4.writeMicroseconds(throttle.getValue() - y_output - x_output);
   } else {
     motor_1.writeMicroseconds(1000);
     motor_2.writeMicroseconds(1000);
+    motor_3.writeMicroseconds(1000);
+    motor_4.writeMicroseconds(1000);
    }
   if (loop_counter++ % 200 == 0) {
-    float tmp = ((float)throttle.getValue()-1000)/100000;
-    if (tmp < 0) tmp = 0;
-    myPID.configure(tmp, 0, dk, 1000, 16, true);
-    Serial.print(tmp, 8);
+    Serial.print(aileron.getValue() - 1500);
+    Serial.print("\t");
+    Serial.print(elevator.getValue() - 1500);
     Serial.print("\t");
     Serial.println(micros()-last);
   }
